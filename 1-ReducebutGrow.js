3b29767587260aed11fc2ee96859b38c6ad362c7
Habibie <60422730+Bee-habibi@users.noreply.github.com>
2024-02-25 13:57:20 +0800

    Update 1-ReducebutGrow.js

--- 1-ReducebutGrow.js
+++ 1-ReducebutGrow.js
@@ -2,6 +2,14 @@
 // ! 1/366
 // https://www.codewars.com/kata/57f780909f7e8e3183000078
 
+// function grow(x){
+//   let result = x[0];
+//   for (let i = 0; i < x.length; i++){
+//     result *= x[i]
+//   }
+//   return result;
+// }
+
 // function grow(x) {
 //   return (result = x.reduce((acc, curr) => acc * curr, 1));
 // }
