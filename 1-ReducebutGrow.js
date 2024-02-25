cfd5afcd057b2c408bc50ac21ae98bcc0f8e9097
Habibie <60422730+Bee-habibi@users.noreply.github.com>
2024-02-25 13:55:08 +0800

    update Desciption for 1/366

--- 1-ReducebutGrow.js
+++ 1-ReducebutGrow.js
@@ -1,27 +1,17 @@
-//?? Cara 1
-// function grow(x) {
-//   // ! ambil index pertama
-//   let result = x[0];
-//   // ! loopig array sebanyak jumlah array
-//   for (let i = 1; i < x.length; i++) {
-//     // ! kali-kan result dengan index setelahnya
-//     result *= x[i];
-//   }
+// ! WPU Coding Challenge 2024
+// ! 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
 
+// function grow(x) {
+//   return (result = x.reduce((acc, curr) => acc * curr, 1));
+// }
 //   return result;
 // }
 
-//?? Cara 2
 // function grow(x) {
 //   return (result = x.reduce((acc, curr) => acc * curr, 1));
-//   // nilai default acc = 1
-//   // curr = index array
-//   // setelah di operasikan maka nilai akan di looping sebanyak index
 // }
 
-// // ?? function expresion arrow
 // const grow = (x) => (result = x.reduce((acc, curr) => acc * curr));
 
-// ?? nemu di codewars hehe
-// const grow = (x) => eval(x.join("*"));
-console.log(grow([2, 2, 2, 2, 2, 2]));
+// console.log(grow([2, 2, 2, 2, 2, 2]));
